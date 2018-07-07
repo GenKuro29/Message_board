e7c654af606e03dcc0e6ebe0968bb054c72ccc96
Gen.K <kamakuraha1192@hotmail.com>
2018-07-07 09:45:04 +0000

    CRUD messages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE thml>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MessageBoard></title>
+    </head>
+    
+    <body>
+        @yield('content')
+    </body>
+</html>
